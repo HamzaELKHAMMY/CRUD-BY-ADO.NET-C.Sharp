f1acd37b1a8e8eaaad4f4e6cad7daa915cd09927
ELKHAMMY Hamza <hamzaelkhammy@gmail.com>
2021-11-11 19:16:23 +0100

    initial commit

---
+++ TP3/Form1.Designer.cs
@@ -0,0 +1,384 @@
+﻿
+namespace TP3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAjouter = new System.Windows.Forms.Button();
+            this.txtCin = new System.Windows.Forms.TextBox();
+            this.txtNom = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtAppliqué = new System.Windows.Forms.Button();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btModefier = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnSupprimer = new System.Windows.Forms.Button();
+            this.btnRechercher = new System.Windows.Forms.Button();
+            this.textCin = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtSexe = new System.Windows.Forms.TextBox();
+            this.txtDatanaissance = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoSize = true;
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.panel1.Controls.Add(this.txtSexe);
+            this.panel1.Controls.Add(this.txtDatanaissance);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnAjouter);
+            this.panel1.Controls.Add(this.txtCin);
+            this.panel1.Controls.Add(this.txtNom);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(779, 168);
+            this.panel1.TabIndex = 0;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, -4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(131, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Ajout d\'un stagiaire";
+            // 
+            // btnAjouter
+            // 
+            this.btnAjouter.AutoSize = true;
+            this.btnAjouter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnAjouter.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAjouter.Location = new System.Drawing.Point(647, 137);
+            this.btnAjouter.Name = "btnAjouter";
+            this.btnAjouter.Size = new System.Drawing.Size(129, 28);
+            this.btnAjouter.TabIndex = 4;
+            this.btnAjouter.Text = "Ajouter";
+            this.btnAjouter.UseVisualStyleBackColor = true;
+            this.btnAjouter.Click += new System.EventHandler(this.btnAjouter_Click);
+            // 
+            // txtCin
+            // 
+            this.txtCin.Location = new System.Drawing.Point(117, 98);
+            this.txtCin.Name = "txtCin";
+            this.txtCin.Size = new System.Drawing.Size(149, 20);
+            this.txtCin.TabIndex = 3;
+            // 
+            // txtNom
+            // 
+            this.txtNom.Location = new System.Drawing.Point(117, 40);
+            this.txtNom.Name = "txtNom";
+            this.txtNom.Size = new System.Drawing.Size(149, 20);
+            this.txtNom.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 98);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "CIN :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nom :";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Controls.Add(this.btModefier);
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.btnSupprimer);
+            this.panel2.Controls.Add(this.btnRechercher);
+            this.panel2.Controls.Add(this.textCin);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Location = new System.Drawing.Point(12, 186);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(776, 252);
+            this.panel2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.groupBox1.Controls.Add(this.txtAppliqué);
+            this.groupBox1.Controls.Add(this.txtC);
+            this.groupBox1.Controls.Add(this.txtN);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.groupBox1.Location = new System.Drawing.Point(535, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(241, 177);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // txtAppliqué
+            // 
+            this.txtAppliqué.AutoSize = true;
+            this.txtAppliqué.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.txtAppliqué.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtAppliqué.Location = new System.Drawing.Point(145, 140);
+            this.txtAppliqué.Name = "txtAppliqué";
+            this.txtAppliqué.Size = new System.Drawing.Size(75, 28);
+            this.txtAppliqué.TabIndex = 10;
+            this.txtAppliqué.Text = "Appliqué";
+            this.txtAppliqué.UseVisualStyleBackColor = true;
+            this.txtAppliqué.Click += new System.EventHandler(this.txtAppliqué_Click);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(68, 104);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(105, 20);
+            this.txtC.TabIndex = 9;
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(68, 46);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(105, 20);
+            this.txtN.TabIndex = 8;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(17, 46);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(45, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Nom :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(17, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 17);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "CIN :";
+            // 
+            // btModefier
+            // 
+            this.btModefier.AutoSize = true;
+            this.btModefier.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btModefier.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btModefier.Location = new System.Drawing.Point(430, 131);
+            this.btModefier.Name = "btModefier";
+            this.btModefier.Size = new System.Drawing.Size(75, 28);
+            this.btModefier.TabIndex = 8;
+            this.btModefier.Text = "modefier";
+            this.btModefier.UseVisualStyleBackColor = true;
+            this.btModefier.Click += new System.EventHandler(this.txtModefier_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 75);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(406, 177);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(3, -3);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(163, 15);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Rechercher un Stagiaire";
+            // 
+            // btnSupprimer
+            // 
+            this.btnSupprimer.AutoSize = true;
+            this.btnSupprimer.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSupprimer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnSupprimer.Location = new System.Drawing.Point(430, 97);
+            this.btnSupprimer.Name = "btnSupprimer";
+            this.btnSupprimer.Size = new System.Drawing.Size(75, 28);
+            this.btnSupprimer.TabIndex = 5;
+            this.btnSupprimer.Text = "Supprimer";
+            this.btnSupprimer.UseVisualStyleBackColor = true;
+            this.btnSupprimer.Click += new System.EventHandler(this.btnSupprimer_Click);
+            // 
+            // btnRechercher
+            // 
+            this.btnRechercher.AutoSize = true;
+            this.btnRechercher.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnRechercher.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnRechercher.Location = new System.Drawing.Point(269, 43);
+            this.btnRechercher.Name = "btnRechercher";
+            this.btnRechercher.Size = new System.Drawing.Size(110, 23);
+            this.btnRechercher.TabIndex = 4;
+            this.btnRechercher.Text = "Rechercher";
+            this.btnRechercher.UseVisualStyleBackColor = true;
+            this.btnRechercher.Click += new System.EventHandler(this.btnRechercher_Click);
+            // 
+            // textCin
+            // 
+            this.textCin.Location = new System.Drawing.Point(96, 43);
+            this.textCin.Name = "textCin";
+            this.textCin.Size = new System.Drawing.Size(134, 20);
+            this.textCin.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(42, 43);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "CIN :";
+            // 
+            // txtSexe
+            // 
+            this.txtSexe.Location = new System.Drawing.Point(556, 95);
+            this.txtSexe.Name = "txtSexe";
+            this.txtSexe.Size = new System.Drawing.Size(126, 20);
+            this.txtSexe.TabIndex = 9;
+            // 
+            // txtDatanaissance
+            // 
+            this.txtDatanaissance.Location = new System.Drawing.Point(556, 40);
+            this.txtDatanaissance.Name = "txtDatanaissance";
+            this.txtDatanaissance.Size = new System.Drawing.Size(126, 20);
+            this.txtDatanaissance.TabIndex = 8;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(484, 98);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(43, 17);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Sexe :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft YaHei", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(423, 40);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(127, 17);
+            this.label9.TabIndex = 6;
+            this.label9.Text = "Data de naissonce :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAjouter;
+        private System.Windows.Forms.TextBox txtCin;
+        private System.Windows.Forms.TextBox txtNom;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnSupprimer;
+        private System.Windows.Forms.Button btnRechercher;
+        private System.Windows.Forms.TextBox textCin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btModefier;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button txtAppliqué;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtSexe;
+        private System.Windows.Forms.TextBox txtDatanaissance;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ TP3/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace TP3
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = new OleDbConnection(@"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\elkhammy\source\repos\TP3\BasseTP3.mdb");
+        OleDbDataAdapter Da;
+        DataTable Dt = new DataTable();
+        OleDbCommand cmd = new OleDbCommand();
+        DataSet ds = new DataSet();
+        OleDbDataReader rd;
+        public Form1()
+        {
+            InitializeComponent();
+            DatagridView();
+        }
+        void DatagridView()
+        {
+            Dt.Clear();
+            Da = new OleDbDataAdapter("Select * from Stagiaire", conn);
+            Da.Fill(Dt);
+            dataGridView1.DataSource = Dt;
+        }
+
+        private void btnAjouter_Click(object sender, EventArgs e)
+        {
+            if (txtNom.Text != "" && txtCin.Text != "" && txtDatanaissance.Text != "" && txtSexe.Text !="")
+            {
+                Da = new OleDbDataAdapter("Select Nom From Stagiaire where Nom = '" + txtNom.Text + "'", conn);
+                Da.Fill(ds);
+                int i = ds.Tables[0].Rows.Count;
+                if (i > 0)
+                {
+                    MessageBox.Show("Nom existant déjà");
+                    ds.Clear();
+                }
+                else
+                {
+
+                    cmd = new OleDbCommand("Insert Into Stagiaire (Nom,CIN,Datedenaissance,Sexe) values ('" + txtNom.Text + "','" + txtCin.Text + "','"+txtDatanaissance.Text+"','"+txtSexe.Text+"')", conn);
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    conn.Close();
+                    DatagridView();
+                    MessageBox.Show("Stagiaire ajouté", "Add", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("S'il vous plait entrer (Nom,CIN,Data de naissance,Sexe", "Vide", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        
+        private void btnRechercher_Click(object sender, EventArgs e)
+        {
+            if (textCin.Text != "")
+            {
+                Da = new OleDbDataAdapter("select * from Stagiaire where CIN = '" + textCin.Text + "'", conn);
+                /*méthode1
+                Da.Fill(ds, "Stagiaire");
+                if (ds.Tables["Stagiaire"].Rows.Count == 0)
+                {
+                   
+                    MessageBox.Show("le stagiaire CIN:"+this.textCin.Text+" , n'existe pas !!");
+                    dataGridView1.DataSource = null;
+                   
+                }
+                else
+                {
+                    
+                    this.btModefier.Visible = true;
+                    this.btnSupprimer.Visible = true;
+                    dataGridView1.DataSource = ds.Tables["Stagiaire"];
+                    
+                }*/
+
+                //methode 2
+                conn.Open();
+                Dt = new DataTable();
+                Da.Fill(Dt);
+                dataGridView1.DataSource = Dt;
+                dataGridView1.Refresh();
+                Da.Fill(ds);
+                int i = ds.Tables[0].Rows.Count;
+                if (i > 0)
+                {
+                    this.btModefier.Visible = true;
+                    this.btnSupprimer.Visible = true;
+                    ds.Clear();
+                }
+                conn.Close();
+            }
+            else
+            {
+                MessageBox.Show("S'il vous plait entrer CIN", "Vide", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+        }
+
+        private void btnSupprimer_Click(object sender, EventArgs e)
+        {
+            try {
+                OleDbCommand cmd = new OleDbCommand("DELETE FROM Stagiaire WHERE CIN = '" + textCin.Text + "'", conn);
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data Deleted", "Deleted", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DatagridView();
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("non supprimer");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.groupBox1.Visible = false;
+            this.btnSupprimer.Visible = false;
+            this.btModefier.Visible = false;
+        }
+        int cool;
+        private void txtModefier_Click(object sender, EventArgs e)
+        {
+            int tr = 0;
+            
+            
+
+                for (int i = 0; i < this.dataGridView1.Rows.Count - 1; i++)
+                {
+                    if (this.dataGridView1.Rows[i].Cells[1].Value.ToString() == textCin.Text)
+                    {
+                        cool = i;
+                        tr = 1;
+                }
+            }
+                if (tr == 0)
+                    MessageBox.Show("cette persson n'existe pas dans la liste");
+                else
+                    this.groupBox1.Visible = true;
+            
+        }
+
+        private void txtAppliqué_Click(object sender, EventArgs e)
+        {
+            this.dataGridView1.Rows[cool].Cells[1].Value = txtC.Text;
+            this.dataGridView1.Rows[cool].Cells[2].Value = txtN.Text;
+            OleDbCommand cmd = new OleDbCommand("DELETE FROM Stagiaire WHERE CIN = '" + textCin.Text + "'", conn);
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Data Modifier", "Modification", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            conn.Close();
+            this.groupBox1.Visible = false;
+            this.btModefier.Visible = false;
+            this.btnSupprimer.Visible = false;
+            this.txtCin.Text = string.Empty;
+            
+        }
+    }
+}
